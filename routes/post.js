5abdf3aac2f429b518aaf2d1c47c54050b9bde9a
skop01 <opkm2002@hanmail.net>
2020-10-06 09:50:27 +0900

    수정중2

--- routes/post.js
+++ routes/post.js
@@ -36,8 +36,8 @@ const upload = multer({
 
 router.post('/img', isLoggedIn, upload.single('img'), (req, res) => {
     console.log(req.file);
-    // const originalUrl = req.file.location;
-    // const url = originalUrl.replace(/\/original\//, '/thumb/')
+    const originalUrl = req.file.location;
+    const url = originalUrl.replace(/\/original\//, '/thumb/')
     res.json({ url: req.file.location });
 });
 
